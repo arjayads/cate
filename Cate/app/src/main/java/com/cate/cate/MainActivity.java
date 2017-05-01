d9a4548eb94c2ec088402949ba002468108d8c0a
Arjay <arjayads@gmail.com>
2017-05-01 18:36:43 +0800

    show random cat facts on startup

--- Cate/app/src/main/java/com/cate/cate/MainActivity.java
+++ Cate/app/src/main/java/com/cate/cate/MainActivity.java
@@ -1,8 +1,6 @@
 package com.cate.cate;
 
-import android.app.Activity;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -16,6 +14,13 @@ import android.webkit.WebView;
 import com.cate.cate.api.ThaCatApiImage;
 import com.cate.cate.api.ThaCatApiResponse;
 import com.cate.cate.api.TheCatAPI;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import org.json.JSONObject;
+
+import java.util.Iterator;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -37,20 +42,23 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        final WebView webView = (WebView) findViewById(R.id.cat_imageView);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                WebView webView = (WebView) findViewById(R.id.cat_imageView);
-                webView.loadData("Please wait...", "text/html; charset=utf-8", "utf-8");
-
                 // TODO: internet availability
+                webView.loadData(getResources().getString(R.string.please_wait), "text/html; charset=utf-8", "utf-8");
                 findTheCat(webView);
             }
         });
 
-        fab.callOnClick();
+        webView.loadData(getResources().getString(R.string.please_wait), "text/html; charset=utf-8", "utf-8");
+
+        randomCatFacts(webView);
     }
 
     @Override
@@ -85,7 +93,7 @@ public class MainActivity extends AppCompatActivity {
             catSize = MainActivity.CAT_SIZE_MEDIUM;
         }
 
-        TheCatAPI theCatAPI = TheCatAPI.retrofit.create(TheCatAPI.class);
+        TheCatAPI theCatAPI = TheCatAPI.retrofitCat.create(TheCatAPI.class);
 
         Call<ThaCatApiResponse> call = theCatAPI.loadCats(MainActivity.CAT_FORMAT_XML, MainActivity.CAT_KEY, catSize);
 
@@ -122,5 +130,52 @@ public class MainActivity extends AppCompatActivity {
                 // TODO: show popup error
             }
         });
+    }
+
+    private void randomCatFacts(final WebView webView) {
+
+        TheCatAPI theCatAPI = TheCatAPI.retrofitFacts.create(TheCatAPI.class);
+
+        Call<String> call = theCatAPI.loadCatFacts(1); // one only
+
+        call.enqueue(new Callback<String>() {
+
+            @Override
+            public void onResponse(Call<String> call, Response<String> response) {
+                try {
+
+                    JsonParser jsonParser = new JsonParser();
+                    JsonObject jo = (JsonObject)jsonParser.parse(response.body());
+
+                    if (jo.get("success") != null && jo.get("success").getAsBoolean()) {
+
+                        JsonArray facts = jo.get("facts").getAsJsonArray();
+                        if (facts != null && facts.size() > 0) {
+
+                            String content = facts.get(0).toString();
+
+                            String html = "<h2>Today's fact about Cate</h2><h3>"+content+"</h3>";
+
+                            setWebView(webView, html);
+                        }
+
+                    } else {
+                        // TODO: show popup error
+                    }
+
+                }catch (Exception e) {
+                    // TODO: show popup error
+                }
+            }
+
+            @Override
+            public void onFailure(Call<String> call, Throwable t) {
+                // TODO: show popup error
+            }
+        });
+    }
+
+    private void setWebView(final WebView webView, String content) {
+        webView.loadData(content, "text/html; charset=utf-8", "utf-8");
     }
 }
--- Cate/app/src/main/java/com/cate/cate/api/TheCatAPI.java
+++ Cate/app/src/main/java/com/cate/cate/api/TheCatAPI.java
@@ -2,19 +2,29 @@ package com.cate.cate.api;
 
 import retrofit2.Call;
 import retrofit2.Retrofit;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 import retrofit2.converter.simplexml.SimpleXmlConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface TheCatAPI {
 
-    String ENDPOINT = "http://thecatapi.com/";
+    String CAT_ENDPOINT = "http://thecatapi.com/api/images/";
+    String FACTS_ENDPOINT = "http://catfacts-api.appspot.com/api/";
 
-    @GET("api/images/get")
+    @GET("get")
     Call<ThaCatApiResponse> loadCats(@Query("format") String format, @Query("api_key") String apiKey, @Query("size") String size);
 
-    public static final Retrofit retrofit = new Retrofit.Builder()
-            .baseUrl(TheCatAPI.ENDPOINT)
+    @GET("facts")
+    Call<String> loadCatFacts(@Query("number") int format);
+
+    public static final Retrofit retrofitCat = new Retrofit.Builder()
+            .baseUrl(TheCatAPI.CAT_ENDPOINT)
             .addConverterFactory(SimpleXmlConverterFactory.create())
+            .build();
+
+    public static final Retrofit retrofitFacts = new Retrofit.Builder()
+            .baseUrl(TheCatAPI.FACTS_ENDPOINT)
+            .addConverterFactory(ScalarsConverterFactory.create())
             .build();
 }
